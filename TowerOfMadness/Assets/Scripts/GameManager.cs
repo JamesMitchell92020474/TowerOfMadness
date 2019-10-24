895d3d508f353ffc239efdaefc39f7f688046890
JamesMitchell92020474 <jamesmitchelldesign1@gmail.com>
2019-10-24 17:53:30 +1300

    Added UI and pickups.

---
+++ TowerOfMadness/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    private float _timeRemaining;
+
+    public float TimeRemaining 
+    {
+        get { return _timeRemaining; }
+        set { _timeRemaining = value; }
+    }
+
+    private float maxTime = 5 * 60; // in seconds
+
+    public int numDiamonds;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeRemaining = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeRemaining -= Time.deltaTime;
+
+        if (TimeRemaining <= 0)
+        {
+            // Do Something for when the time runs out
+        }
+    }
+}
---
+++ TowerOfMadness/Assets/Scripts/UpdateUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text timerLabel;
+
+    [SerializeField]
+    private Text scoreLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerLabel.text = FormatTime(GameManager.Instance.TimeRemaining);
+        scoreLabel.text = "Score: " + GameManager.Instance.score.ToString();
+    }
+
+    private string FormatTime(float timeInSeconds)
+    {
+        return string.Format("Time: {0}:{1:00}", Mathf.FloorToInt(timeInSeconds / 60), Mathf.FloorToInt(timeInSeconds % 60));
+    }
+}
